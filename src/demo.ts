5d5e32548176a0664aa66956ceb809e53c75d725
Aaron Faanes <dafrito@gmail.com>
2022-12-29 06:34:14 -0800

    Fix demo

--- src/demo.ts
+++ src/demo.ts
@@ -88,7 +88,7 @@ class Scene extends AbstractScene {
         ctx.fillRect(x, y, 1, 1);
       }
     }
-    this._labels.render(proj, this._cam.x(), this._cam.y(), this._cam.scale());
+    this._labels.render(proj, this._cam.x(), this._cam.y(), this._cam.width(), this._cam.height(), this._cam.scale());
 
     return needsUpdate;
   }
