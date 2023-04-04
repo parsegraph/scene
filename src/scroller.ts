a103086561686ae76f9993bb43929085ef09c002
Aaron Faanes <dafrito@gmail.com>
2023-04-04 04:13:16 -0700

    Create scrolling demo

--- src/scroller.ts
+++ src/scroller.ts
@@ -3,74 +3,18 @@ import TimingBelt from "parsegraph-timingbelt";
 import AbstractScene from "./AbstractScene";
 import { WorldLabels } from "./WorldLabel";
 import Color from "parsegraph-color";
-import { InputViewport, Viewport } from "./viewport";
+import { InputViewport, Viewport} from "./viewport";
+
 import Camera, {containsAny} from "parsegraph-camera";
 import {BasicMouseController, KeyController, Keystroke} from "parsegraph-input";
+import ViewportKeyController from "./viewport/key";
 
 const font = "96px sans-serif";
 
-interface Brush {
-  draw(map: TileMap, selectedX: number, selectedY: number, c: Color): void;
-}
-
-class PaintBrush implements Brush {
-  _brushSize: number;
-  _color: Color;
-
-  constructor(brushSize: number = 16, color: Color = null) {
-    this._brushSize = brushSize;
-    this._color = color;
-  }
-
-  draw(map: TileMap, selectedX: number, selectedY: number, c: Color): void {
-    map.get(selectedX, selectedY).setColor(c);
-
-    const brushSize = this._brushSize;
-    for(let bx = 0; bx < 2 * brushSize; ++bx) {
-      for(let by = 0; by < 2 * brushSize; ++by) {
-        const px = selectedX - brushSize + bx;
-        const py = selectedY - brushSize + by;
-
-        if (!map.contains(px, py)) {
-          continue;
-        }
-
-        const lerp = (distance(selectedX, selectedY, px, py) / brushSize);
-        const mixed = (this._color ?? c).clone().interpolate(map.get(px, py).color(), lerp);
-        map.get(px, py).setColor(mixed);
-      }
-    }
-  }
-}
-
-class PencilBrush implements Brush {
-  _brushSize: number;
-  _color: Color;
-
-  constructor(brushSize: number = 1, color: Color = null) {
-    this._brushSize = brushSize;
-    this._color = color;
-  }
-
-  draw(map: TileMap, selectedX: number, selectedY: number, c: Color): void {
-    const brushSize = this._brushSize;
-    for(let bx = 0; bx < 2 * brushSize; ++bx) {
-      for(let by = 0; by < 2 * brushSize; ++by) {
-        const px = selectedX - brushSize + bx;
-        const py = selectedY - brushSize + by;
-
-        if (!map.contains(px, py)) {
-          continue;
-        }
-
-        map.get(px, py).setColor(this._color ?? c);
-      }
-    }
-  }
-}
-
 interface TileMap {
   get(x: number, y:number): Tile;
+  movePlayer(dx: number): void;
+  jumpPlayer(dy: number): void;
   contains(x: number, y:number): boolean;
   tileSize(): number;
   tileWidth(): number;
@@ -79,30 +23,37 @@ interface TileMap {
   markDirty(): void;
 }
 
+interface TileType {
+  color(): Color;
+}
+
 class Tile {
-  _color: Color;
   _x: number;
   _y: number;
   _map: TileMap;
-
-  color() {
-    return this._color;
-  }
-
-  setColor(color: Color) {
-    this._color = color;
-    this.map().markDirty();
-  }
+  _tileType: TileType;
 
   map() {
     return this._map;
   }
 
-  constructor(tileMap: TileMap, x: number, y: number) {
+  constructor(tileMap: TileMap, x: number, y: number, tileType: TileType) {
     this._map = tileMap;
     this._x = x;
     this._y = y;
-    this._color = Color.random(0.5, 1);
+    this._tileType = tileType;
+  }
+
+  type() {
+    return this._tileType;
+  }
+
+  setType(tileType: TileType) {
+    if (this._tileType === tileType) {
+      return;
+    }
+    this._tileType = tileType;
+    this.map().markDirty();
   }
 
   x() {
@@ -114,11 +65,33 @@ class Tile {
   }
 }
 
+class BasicTileType implements TileType {
+  _color: Color;
+  constructor(color: Color) {
+    this._color = color;
+  }
+
+  color() {
+    return this._color;
+  }
+
+  setColor(color: Color) {
+    this._color = color;
+  }
+}
+
+const SKY = new BasicTileType(new Color(0, 0, 1, 1));
+const GROUND = new BasicTileType(new Color(0.5, 0.5, 0, 1));
+
 class TileScene extends AbstractScene implements TileMap {
   _camera: Camera;
 
   _tiles: Tile[][];
   _tileSize: number;
+
+  _playerPos: [number, number];
+
+  _lastTick: number;
 
   constructor(projector: Projector, tileSize: number, tileWidth: number, tileHeight: number) {
     super(projector);
@@ -130,9 +103,46 @@ class TileScene extends AbstractScene implements TileMap {
       const column:Tile[] = [];
       this._tiles.push(column);
       for(let y = 0; y < tileHeight; ++y) {
-        column.push(new Tile(this, x, y));
+        column.push(new Tile(this, x, y, (x % 5 === 0 || y % 2 === 0) ? SKY : GROUND));
       }
     }
+    this._playerPos = [0, 0];
+
+    this._lastTick = null;
+  }
+
+  movePlayer(dx: number) {
+    if (dx > 0) {
+      if (this.getPlayerForward()?.type() === GROUND) {
+        return;
+      }
+      this._playerPos[0] += dx;
+      if (this.getPlayerForward()?.type() === GROUND) {
+        this._playerPos[0] -= this._playerPos[0] - Math.floor(this._playerPos[0]);
+      }
+    }
+    if (dx < 0) {
+      if (this.getPlayerBackward()?.type() === GROUND) {
+        return;
+      }
+      this._playerPos[0] += dx;
+      if (this.getPlayerBackward()?.type() === GROUND) {
+        this._playerPos[0] += Math.ceil(this._playerPos[0]) - this._playerPos[0];
+      }
+    }
+    this.markDirty();
+  }
+
+  jumpPlayer(dy: number) {
+    if (dy > 0) {
+      dy = Math.min(dy, this.getPlayerCeiling());
+      if (dy === 0) {
+        console.log("CEILING HIT");
+        return;
+      }
+    }
+    this._playerPos[1] += -dy;
+    this.markDirty();
   }
 
   tileWidth() {
@@ -162,12 +172,54 @@ class TileScene extends AbstractScene implements TileMap {
   }
 
   get(x: number, y:number) {
-    return this._tiles[x][y];
+    return this._tiles[x]?.[y];
   }
 
-  set(x: number, y:number, color: Color) {
-    this.get(x, y).setColor(color);
+  set(x: number, y:number, type: TileType) {
+    this.get(x, y).setType(type);
     this.scheduleUpdate();
+  }
+
+  getPlayerCeiling() {
+    let d = 0;
+    if (this.get(Math.floor(this._playerPos[0]), Math.floor(this._playerPos[1]))?.type() === SKY) {
+      d += this._playerPos[1] % 1;
+    } else {
+      return 0;
+    }
+    for(let i = 0; (this.get(Math.floor(this._playerPos[0]), Math.floor(this._playerPos[1] - i))?.type() === SKY) ; --i) {
+      d += 1;
+    }
+    return d;
+  }
+
+  getPlayerForward() {
+    return this.get(Math.floor(this._playerPos[0]) + 1, Math.floor(this._playerPos[1]));
+  }
+
+  getPlayerBackward() {
+    return this.get(Math.ceil(this._playerPos[0]) - 1, Math.ceil(this._playerPos[1]));
+  }
+
+  getPlayerGround() {
+    return this.get(Math.floor(this._playerPos[0]), Math.floor(this._playerPos[1]) + 1);
+  }
+
+  tick() {
+    const now = Date.now();
+    if (!this._lastTick) {
+      this._lastTick = now;
+      return false;
+    }
+    const elapsed = now - this._lastTick;
+    this._lastTick = now;
+    if (this.getPlayerGround()?.type() === SKY) {
+      this.jumpPlayer(-elapsed / 1000);
+      if (this.getPlayerGround()?.type() === GROUND) {
+        this.jumpPlayer(this._playerPos[1] - Math.floor(this._playerPos[1]));
+      }
+    }
+    return false;
   }
 
   paint() {
@@ -181,10 +233,10 @@ class TileScene extends AbstractScene implements TileMap {
 
     const ctx = proj.overlay();
     const wt = this.worldTransform();
+    const ts = this.tileSize();
 
     this._tiles.forEach(column => {
       column.forEach(tile => {
-        const ts = this.tileSize();
         const cx = tile.x() * ts;
         const cy = tile.y() * ts;
 
@@ -196,10 +248,25 @@ class TileScene extends AbstractScene implements TileMap {
           cx + ts/2, cy + ts/2, ts, ts)) {
           return;
         }
-        ctx.fillStyle = tile.color().asRGBA();
+        ctx.fillStyle = tile.type().color().asRGBA();
         ctx.fillRect(tile.x() * ts, tile.y() * ts, ts, ts);
       });
     });
+
+    const [playerX, playerY] = this._playerPos;
+    ctx.fillStyle = "green";
+    ctx.fillRect(ts * playerX, ts * playerY, ts, ts);
+
+    ctx.resetTransform();
+    ctx.font = "24px mono";
+    ctx.textBaseline = "top";
+    ctx.textAlign = "left";
+    ctx.fillStyle = "white";
+    ctx.fillText(`Player: ${this._playerPos[0]}, ${this._playerPos[1]}`, 0, 0);
+    ctx.fillText(`Ceiling: ${this.getPlayerCeiling()}`, 0, 28 * 1);
+    ctx.fillText(`Ground: ${this.getPlayerGround()?.type() === SKY ? "SKY" : "GROUND"}`, 0, 28 * 2);
+    ctx.fillText(`Backward: ${this.getPlayerBackward()?.type() === SKY ? "SKY" : "GROUND"}`, 0, 28 * 3);
+    ctx.fillText(`Forward: ${this.getPlayerForward()?.type() === SKY ? "SKY" : "GROUND"}`, 0, 28 * 4);
 
     return needsUpdate;
   }
@@ -215,113 +282,37 @@ const distance = (ax:number, ay:number, bx:number, by:number) => {
   );
 }
 
-interface BrushSink {
-  setBrush(brush: Brush): void;
-}
-
-class ScrollerKeyController implements KeyController {
-  keydowns: { [id: string]: Date };
-  _viewport: InputViewport;
-  _next: KeyController;
-
-  _paintBrush: Brush;
-  _pencilBrush: Brush;
-
-  _sink: BrushSink;
-
-  constructor(sink: BrushSink, viewport: InputViewport) {
-    // A map of keyName's to a true value.
-    this.keydowns = {};
-    this._sink = sink;
-    this._viewport = viewport;
-
-    this._paintBrush = new PaintBrush();
-    this._pencilBrush = new PencilBrush();
+class ScrollerKeyController extends ViewportKeyController {
+  constructor(viewport: InputViewport, map: TileMap) {
+    super(viewport);
+    this._map = map;
   }
-
-  next() {
-    return this._next;
+  map() {
+    return this._map;
   }
-
-  setNext(next: KeyController) {
-    this._next = next;
-  }
-
-  lastMouseX() {
-    return this.viewport().lastMouseX();
-  }
-
-  lastMouseY() {
-    return this.viewport().lastMouseY();
-  }
-
-  getKey(key: string) {
-    return this.keydowns[key] ? 1 : 0;
-  }
-
-  sink() {
-    return this._sink;
-  }
-
-  keyElapsed(key: string, t: Date) {
-    const v = this.keydowns[key];
-    if (!v) {
-      return 0;
+  _map: TileMap;
+  update(t: Date) {
+    let needsUpdate = super.update(t);
+    const speed = 2;
+    if (this.getKey("a")) {
+      this.map().movePlayer(-speed * this.keyElapsed("a", t));
     }
-    const elapsed = (t.getTime() - v.getTime()) / 1000;
-    this.keydowns[key] = t;
-    return elapsed;
-  }
-
-  scheduleRepaint() {
-    this.viewport().scheduleRepaint();
-  }
-
-  keydown(event: Keystroke) {
-    switch(event.key()) {
-    case "1":
-      this.sink().setBrush(this._paintBrush);
-      return true;
-    case "2":
-      this.sink().setBrush(this._pencilBrush);
-      return true;
-    case "3":
-      this.sink().setBrush(new PencilBrush(1, new Color(0, 0, 0, 1)));
-      return true;
-    case "4":
-      this.sink().setBrush(new PaintBrush(8, new Color(1, 1, 1, 1)));
-      return true;
+    if (this.getKey("d")) {
+      this.map().movePlayer(speed * this.keyElapsed("d", t));
     }
-    return false;
-  }
-
-  keyup(_event: Keystroke) {
-    return false;
-  }
-
-  viewport() {
-    return this._viewport;
-  }
-
-  camera() {
-    return this.viewport().camera();
-  }
-
-  update(_t: Date) {
-    return false;
+    if (this.getKey(" ")) {
+      this.map().jumpPlayer(speed * this.keyElapsed(" ", t));
+    }
+    return needsUpdate;
   }
 }
 
 class ScrollerMouseController extends BasicMouseController {
   _map: TileMap;
 
-  _drawing: boolean;
-
   constructor(map: TileMap) {
     super();
     this._map = map;
-    this._drawing = false;
-    this._brush = new PaintBrush(1);
   }
 
   map() {
@@ -329,63 +320,14 @@ class ScrollerMouseController extends BasicMouseController {
   }
 
   mousemove(x: number, y: number): boolean {
-    if (!this._drawing) {
-      return false;
-    }
-    this.draw(x, y);
-  }
-
-  _brush: Brush;
-  setBrush(brush: Brush) {
-    this._brush = brush;
-  }
-
-  draw(x: number, y: number): boolean {
-    const ts = this.map().tileSize();
-
-    const [worldX, worldY] = this.map().camera().transform(x, y);
-
-    const tw = this.map().tileWidth();
-    const th = this.map().tileHeight();
-
-    if (!containsAny(
-      ts * tw/2,
-      ts * th/2,
-      ts * tw,
-      ts * th,
-      worldX,
-      worldY,
-      1,1
-    )) {
-      return false;
-    }
-
-    const selectedX = Math.floor(worldX / ts);
-    const selectedY = Math.floor(worldY / ts);
-    const c = Color.random(1, 1);
-    this._brush.draw(this.map(), selectedX, selectedY, c);
-    return true;
+    return false;
   }
 
   mouseup() {
-    if (!this._drawing) {
-      return false;
-    }
-    this._drawing = false;
-    return true;
+    return false;
   }
 
   mousedown(button: any, downStart: number, x: number, y: number) {
-    if (button != "2") {
-      console.log("Not handled");
-      return false;
-    }
-
-    super.mousedown(button, downStart, x, y);
-    if (this.draw(x, y)) {
-      this._drawing = true;
-      return true;
-    }
     return false;
   }
 }
@@ -396,16 +338,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const belt = new TimingBelt();
   const proj = new BasicProjector();
   proj.container().tabIndex = 0;
-  const scene = new TileScene(proj, 100, 500, 300);
+  const scene = new TileScene(proj, 100, 256, 64);
 
   const viewport = new Viewport(new Color(0.2, 0.2, 0.2, 1));
 
   const mouse = new ScrollerMouseController(scene);
   viewport.mouse().setNext(mouse);
-  const key = new ScrollerKeyController(mouse, viewport);
+  const key = new ScrollerKeyController(viewport, scene);
   viewport.key().setNext(key);
   viewport.setScene(scene);
   scene.setCamera(viewport.camera());
+
+  setInterval(()=>{
+    scene.markDirty();
+  }, 100);
 
   belt.addRenderable(viewport);
   root.appendChild(proj.container());
