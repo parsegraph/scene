ed1a42bf1afe20352ec0c3e5d7bb2bbbe5c48f8d
Aaron Faanes <dafrito@gmail.com>
2023-04-08 06:07:06 -0700

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const {webpackConfig, relDir} = require("./webpack.common");
 module.exports = {
   entry: {
     index: relDir("src/index.ts"),
-    demo: relDir("src/demo/scene.ts"),
+    demo: relDir("src/demo.ts"),
+    scene: relDir("src/demo/scene.ts"),
     drawing: relDir("src/demo/drawing.ts"),
     scroller: relDir("src/demo/scroller.ts"),
   },
